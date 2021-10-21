8c688814c2b1ab7d6e0da8e312da1f0a597bb3bf
Francisco Resendiz Camacho <fresendiz@axtel.com.mx>
2021-10-21 18:10:29 -0500

    feat: Proyecto base alertas

---
+++ src/main/groovy/org/palina/alerts/AlertsApplication.groovy
@@ -0,0 +1,13 @@
+package org.palina.alerts
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AlertsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(AlertsApplication, args)
+	}
+
+}
---
+++ src/test/groovy/org/palina/alerts/AlertsApplicationTests.groovy
@@ -0,0 +1,13 @@
+package org.palina.alerts
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
